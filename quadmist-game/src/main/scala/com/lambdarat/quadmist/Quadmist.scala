f75300f71e80bc253c589f045fcb7fcfe5d88ce9
Roberto Serrano <r.semarcos@gmail.com>
2020-11-26 19:05:39 +0100

    Add more assets, refactor board presentation

--- quadmist-game/src/main/scala/com/lambdarat/quadmist/Quadmist.scala
+++ quadmist-game/src/main/scala/com/lambdarat/quadmist/Quadmist.scala
@@ -14,12 +14,19 @@ object Quadmist extends IndigoGame[Unit, Unit, Unit, Unit] {
   object Assets {
     val cardName = AssetName("card")
     val cardAsset = AssetType.Image(cardName, AssetPath(s"$baseUrl/card.png"))
+
+    val blockName = AssetName("block")
+    val blockAsset = AssetType.Image(blockName, AssetPath(s"$baseUrl/block.png"))
+
+    val freeName = AssetName("free")
+    val freeAsset = AssetType.Image(freeName, AssetPath(s"$baseUrl/free.png"))
+
+    val assets: Set[AssetType] = Set(cardAsset, blockAsset, freeAsset)
   }
 
-  def boot(flags: Map[String, String]): BootResult[Unit] = {
+  def boot(flags: Map[String, String]): BootResult[Unit] =
     BootResult(GameConfig.default.withViewport(1024, 768).withClearColor(ClearColor.White), ())
-      .withAssets(Assets.cardAsset)
-  }
+      .withAssets(Assets.assets)
 
   def initialScene(bootData: Unit): Option[SceneName] =
     Some(EmptyScene.name)
@@ -79,36 +86,26 @@ object EmptyScene extends Scene[Unit, Unit, Unit] {
     val hMargin = 26
     val vMargin = 6
 
-    def buildCard: Graphic = Graphic(Rectangle(0, 0, 140, 180), 1, Material.Textured(Assets.cardName))
+    def buildCard: Graphic = Graphic(Rectangle(0, 0, 140, 180), 1, Material.Textured(Assets.freeName))
 
-    // First row
-    val card00 = buildCard.moveTo(cardNextColumn(1) + hMargin, cardNextRow(1) + vMargin)
-    val card01 = buildCard.moveTo(cardNextColumn(2) + hMargin, cardNextRow(1) + vMargin)
-    val card02 = buildCard.moveTo(cardNextColumn(3) + hMargin, cardNextRow(1) + vMargin)
-    val card03 = buildCard.moveTo(cardNextColumn(4) + hMargin, cardNextRow(1) + vMargin)
+    def buildRow(rowIdx: Int): List[Graphic] = {
+      val square1 = buildCard.moveTo(cardNextColumn(1) + hMargin, cardNextRow(rowIdx) + vMargin)
+      val square2 = buildCard.moveTo(cardNextColumn(2) + hMargin, cardNextRow(rowIdx) + vMargin)
+      val square3 = buildCard.moveTo(cardNextColumn(3) + hMargin, cardNextRow(rowIdx) + vMargin)
+      val square4 = buildCard.moveTo(cardNextColumn(4) + hMargin, cardNextRow(rowIdx) + vMargin)
 
-    // Second row
-    val card10 = buildCard.moveTo(cardNextColumn(1) + hMargin, cardNextRow(2) + vMargin)
-    val card11 = buildCard.moveTo(cardNextColumn(2) + hMargin, cardNextRow(2) + vMargin)
-    val card12 = buildCard.moveTo(cardNextColumn(3) + hMargin, cardNextRow(2) + vMargin)
-    val card13 = buildCard.moveTo(cardNextColumn(4) + hMargin, cardNextRow(2) + vMargin)
+      List(square1, square2, square3, square4)
+    }
 
-    // Third row
-    val card20 = buildCard.moveTo(cardNextColumn(1) + hMargin, cardNextRow(3) + vMargin)
-    val card21 = buildCard.moveTo(cardNextColumn(2) + hMargin, cardNextRow(3) + vMargin)
-    val card22 = buildCard.moveTo(cardNextColumn(3) + hMargin, cardNextRow(3) + vMargin)
-    val card23 = buildCard.moveTo(cardNextColumn(4) + hMargin, cardNextRow(3) + vMargin)
-
-    // Fourth row
-    val card30 = buildCard.moveTo(cardNextColumn(1) + hMargin, cardNextRow(4) + vMargin)
-    val card31 = buildCard.moveTo(cardNextColumn(2) + hMargin, cardNextRow(4) + vMargin)
-    val card32 = buildCard.moveTo(cardNextColumn(3) + hMargin, cardNextRow(4) + vMargin)
-    val card33 = buildCard.moveTo(cardNextColumn(4) + hMargin, cardNextRow(4) + vMargin)
-
-    val cards = List(card00, card01, card02, card03, card10, card11, card12, card13, card20, card21, card22, card23, card30, card31, card32, card33)
+    val squares = List(
+      buildRow(1),
+      buildRow(2),
+      buildRow(3),
+      buildRow(4)
+    ).flatten
 
     SceneUpdateFragment.empty
-      .addGameLayerNodes(cards)
+      .addGameLayerNodes(squares)
   }
 
 }
